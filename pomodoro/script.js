894597ba5a68b2631a8d7945ca77209d67e0d313
karan shah <119512863+karanshah675@users.noreply.github.com>
2026-08-05 00:14:10 +0530

    Update script.js

--- pomodoro/script.js
+++ pomodoro/script.js
@@ -1,19 +1,27 @@
 const btn = document.querySelector(".start-btn");
 const timerStartStop = document.querySelector(".timer");
 const timer = document.querySelector(".time");
-const changeBtn = document.querySelector(".change-page")
-const timerSetup =document.querySelector(".timer-setup")
+const changeBtn = document.querySelector(".change-page");
+const timerSetup = document.querySelector(".timer-setup");
+const editTime = document.querySelectorAll(".time-edit-btn");
+const sessionTimeEdit = document.querySelector(".session");
+const breakTimeEdit = document.querySelector(".break");
+let forSession = document.getElementById("forSession").value;
+let forBreak = document.getElementById("forBreak").value;
 let second = 58;
 let minute = 0;
 let check = 0;
+let sessionTime = 25;
+let breakTime = 5;
 let intervalId;
- 
+
 btn.addEventListener("click", () => {
   btn.classList.remove("animate");
-
   // Force browser to reflow
   void btn.offsetWidth;
   btn.classList.add("animate");
+  second = 0;
+  minute = 0;
 });
 timerStartStop.addEventListener("click", (e) => {
   if (!check) {
@@ -31,6 +39,53 @@ timerStartStop.addEventListener("click", (e) => {
     check = 0;
   }
 });
-changeBtn.addEventListener("click",()=>{
-        timerSetup.style.opecity=1
-})
+
+changeBtn.addEventListener("click", () => {
+  changeBtn.classList.remove("animate");
+  // Force browser to reflow
+  void changeBtn.offsetWidth;
+  changeBtn.classList.add("animate");
+  // timerSetup.style.visibility="visible"
+  timerSetup.classList.toggle("visiblee");
+});
+document.querySelectorAll(".edit").forEach((e) => {
+  e.addEventListener("click", (el) => {
+    if (forSession == "on" && forBreak!="true") {
+        e.style.backgroundColor = "aqua";
+        e.style.color = "black";
+        forSession= "true";
+    } 
+    else {
+      e.style.color = "white";
+      e.style.backgroundColor = "black";
+      forSession= "on";
+    }
+    if (forBreak == "on" && forSession!="true") {
+       
+        e.style.backgroundColor = "aqua";
+        e.style.color = "black";
+        
+        forBreak= "true";
+      
+    } 
+    else {
+      e.style.color = "white";
+      e.style.backgroundColor = "black";
+      forBreak= "on";
+    }
+  });
+});
+editTime.forEach((e) => {
+  e.addEventListener("click", (el) => {
+    // console.log(el.target.classList);
+    if (forSession == "true") {
+      sessionTime++;
+      sessionTimeEdit.children[1].innerHTML = `${sessionTime} min`;
+    }
+    if (forBreak == "true") {
+      breakTime++;
+      breakTimeEdit.children[1].innerHTML = `${breakTime} min`;
+    }
+
+  });
+});
